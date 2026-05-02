faf5a4dc07eec71d8736354e56db13822428e0b6
jmay27694-alt <jmay27694@gmail.com>
2026-05-02 14:20:56 -0500

    Update main.py
    Fixed

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from controller import BankController
 
-
-def main() -> None:
-    """Run the bank account app."""
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = BankController()
     window.show()
     sys.exit(app.exec())
-
-
-if __name__ == "__main__":
-    main()
